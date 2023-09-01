790867273a78d515776a15385201c748001d95e4
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-09-02 04:26:00 +0600

    set up setting and link templates and apps

--- django_hospital_management_system/settings.py
+++ django_hospital_management_system/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'patient_details',
+    'doctor_profile',
+    'doctors',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'django_hospital_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- patient_details/views.py
+++ patient_details/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def patient_details(request):
+    return render(request,'patient_detaisls')
