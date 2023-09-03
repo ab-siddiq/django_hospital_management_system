13e504cae81311c9f3351fd0472ae277451c86b7
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-09-03 20:39:41 +0600

    add models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import UserDetails,UserHospitalManagement
 # Register your models here.
+class UserHospitalManagementAdmin(admin.ModelAdmin):
+    list_display = ('username','account_type','employee_id','date_of_birth','gender')
+admin.site.register(UserHospitalManagement,UserHospitalManagementAdmin)
+admin.site.register(UserDetails)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+ACCOUNT_TYPE = (
+    ('Doctor','Doctor'),
+    ('Patient','Patient'),
+    ('Admin','Admin'),
+    ('User','User'),
+    ('Technologist','Technologist'),
+    ('Recieptionist','Recietionist'),
+    ('Accounce','Accounce'),
+    ('Cash','Cash'),
+)
+GENDER_TYPE = (
+    ('Male','Male'),
+    ('Female','Female'),
+    ('Other','Other'),
+)
+class UserHospitalManagement(models.Model):
+    username = models.OneToOneField(User,related_name='account',on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=20,choices=ACCOUNT_TYPE)
+    employee_id = models.AutoField(primary_key=True)
+    date_of_birth = models.DateField(null=True,blank=True)
+    gender = models.CharField(max_length=10,choices=GENDER_TYPE)
+    
+ 
+    
+class UserDetails(models.Model):
+    username = models.OneToOneField(User,related_name='user_details',on_delete=models.CASCADE)
+    house = models.CharField(max_length=1000,null=True,blank=True)
+    city = models.CharField(max_length=100,null=True,blank=True)
+    country = models.CharField(max_length=100,null=True,blank=True)
+    mobile = models.IntegerField()
+
